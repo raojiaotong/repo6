77f109c2d35350aff4c251f0c382537bd8e54725
rjiaotong <1013350421@qq.com>
2020-11-21 17:30:36 +0800

    1,修改文件内容！

--- git-test-project/src/com/tongge/git/FirstClass.java
+++ git-test-project/src/com/tongge/git/FirstClass.java
@@ -4,4 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git！");
     }
+
+    public void test(){
+        int i =0;
+        i++;
+    }
 }
